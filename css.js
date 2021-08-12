4093d60422d2458000bbe2058604be4783fe107f
Roberto Samudio <robe.arch@gmail.com>
2021-08-12 12:07:02 -0500

    add font

--- css.js
+++ css.js
@@ -11,6 +11,7 @@ html,
 
 
 body{
+    font-family: 'Poppins', sans-serif;
     background: hsla(261, 77%, 55%, 1);
     background: linear-gradient(90deg, hsla(261, 77%, 55%, 0.8) 0%, hsla(239, 100%, 71%, 0.9) 50%, hsla(189, 86%, 64%, 1) 100%);
     background: -moz-linear-gradient(90deg, hsla(261, 77%, 55%, 0.8) 0%, hsla(239, 100%, 71%, 0.9) 50%, hsla(189, 86%, 64%, 1) 100%);
