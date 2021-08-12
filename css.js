92c170cbbffac65da0056e207e41d335622abc29
Roberto Samudio <robe.arch@gmail.com>
2021-08-12 12:03:10 -0500

    Added css

---
+++ css.js
@@ -0,0 +1,25 @@
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap');
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+}
+
+body,
+html,
+
+
+body{
+    background: hsla(261, 77%, 55%, 1);
+    background: linear-gradient(90deg, hsla(261, 77%, 55%, 0.8) 0%, hsla(239, 100%, 71%, 0.9) 50%, hsla(189, 86%, 64%, 1) 100%);
+    background: -moz-linear-gradient(90deg, hsla(261, 77%, 55%, 0.8) 0%, hsla(239, 100%, 71%, 0.9) 50%, hsla(189, 86%, 64%, 1) 100%);
+    background: -webkit-linear-gradient(90deg, hsla(261, 77%, 55%, 0.8) 0%, hsla(239, 100%, 71%, 0.9) 50%, hsla(189, 86%, 64%, 1) 100%);
+    filter: progid: DXImageTransform.Microsoft.gradient( startColorstr="#7436E5", endColorstr="#6C6FFF", GradientType=1 );
+}
+
+.heading{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
